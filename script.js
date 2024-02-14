713b4c7a7dcb48a35d31466f88a84125bfe22ce7
Fardeen Khan <fk7483136@gmail.com>
2024-02-14 10:56:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// add an event listener to hamburger
+
+document.querySelector(".hamburger").addEventListener("click",()=>{
+    document.querySelector(".left").style.left = "0"
+})
+
+document.querySelector(".close").addEventListener('click',()=>{
+    document.querySelector(".left").style.left = "-120%"
+})
